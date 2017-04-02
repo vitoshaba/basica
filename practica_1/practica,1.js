ba0fa59f1c1a3057e4351e3fe71b1f330e2a79ae
vitoshaba <sebas@elektrodx.com>
2017-04-02 15:50:11 -0700

    primer commit

---
+++ practica_1/practica,1.js
@@ -0,0 +1,16 @@
+var euro=1.08;
+var peso_mexicano=18.72; 
+var real=3.16;
+var dolar canadinse=0.75;
+var dolar australiano=0.76;
+var yen japones=0.0089;
+var libra esterlina=1.25;
+var franco suizo=0.99;
+var moneda=parseFloat(prompt("¿Que moneda quiesres? 1 es euro ");
+var texto=document.getElementById("texto");
+  var final=moneda*texto,
+if(moneda==1)
+{
+ moneda = euro
+
+}
---
+++ prueba/practica,1.js
@@ -0,0 +1,9 @@
+var euro=1.08;
+var peso_mexicano=18.72; 
+var real=3.16;
+var dolar canadinse=0.75;
+var dolar australiano=0.76;
+var yen japones=0.0089;
+var libra esterlina=1.25;
+var franco suizo=0.99;
+alert("si funciona");
